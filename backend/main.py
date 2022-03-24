1d62dd8aab002e5d5b3ea06dd80c40c1299039a6
Nguyễn Lâm <nguyenlam@Nguyens-MacBook-Pro.local>
2022-03-24 23:17:27 +0300

    update advance part

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from distutils.log import error
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
+    return redirect(url_for("profile"))
+
+@app.route("/profile")
+def profile():
     return render_template("index.html")
 
-@app.route('/profile')
-def profile():
-    return render_template("profile.html")
+@app.route("/static/<path:file_path>")
+def files(file_path):
+    return url_for('static', filename=file_path)
 
 app.run(host="localhost", port=5001, debug=True)
